25a06d01159f3f5abb426d236d121e302ec01824
DMadhuranga <kdanushka8@gmail.com>
2017-12-19 10:01:41 +0530

    Initial project

---
+++ dist/dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+try {
+$dbh = new PDO('mysql:host=localhost;dbname=kalum_timber', 'root', '1010');
+}catch(PDOException $e){
+	die("Connection error");
+}
+if(!$dbh){
+	die("Connection error");
+}
+
+?>
